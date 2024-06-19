709dfbe886c82a977b32b399792f7502c588da8a
Rose Boveto <roseboveto@gmail.com>
2024-06-19 17:57:31 -0300

    aulda de java1

---
+++ Aula/index.js
@@ -0,0 +1,26 @@
+console.log('Hello World')
+console.log('Aula Java')
+//utilizar sempre o Camel case
+var nomeDoProfessor = 'Roselene'; //usar sempre aspas string
+var idade = 39; // number
+var alturaDoAluno = 1.63; // number
+var peso = 75.5; // number
+var estudando = true // bolean verdadeiro ou falso
+
+console.log(nomeDoProfessor, typeof nomeDoProfessor)
+// console.log(typeof nomeDoProfessor)
+console.log(idade, typeof idade);
+console.log(alturaDoAluno, typeof alturaDoAluno);
+console.log(peso, typeof peso);
+console.log(estudando, typeof estudando);
+// não usar var no Java script
+let notaDoAluno = 10
+const medDoAluno = 8
+notaDoAluno = 9
+
+console.log(notaDoAluno, medDoAluno)
+
+
+
+
+
---
+++ Aula/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const numero = 16;
+
+console.log("raiz quadrada:", numero, "é igual",Math.sqrt(numero));
